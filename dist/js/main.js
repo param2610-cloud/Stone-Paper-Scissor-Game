f6c8e7d83c131efe20878a75dac0048cfb3c87f5
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2023-12-11 18:49:56 +0530

    main

--- dist/js/main.js
+++ dist/js/main.js
@@ -241,7 +241,7 @@ const resetBoard = () => {
 
 const createGameImage = (icon, appendToElement) => {
     const image = document.createElement("img");
-    image.src = `img/${icon}.png`;
+    image.src = `dist/img/${icon}.png`;
     image.alt = icon;
     appendToElement.appendChild(image);
 }
